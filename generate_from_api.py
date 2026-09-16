d67237667545ea6e739d8b382593facc38bacc7f
cynkpawel <cynk.pawel@zngroup.pl>
2026-09-16 23:49:31 +0200

    Update generate_from_api.py

--- generate_from_api.py
+++ generate_from_api.py
@@ -59,9 +59,6 @@ def build_pigu_xml(products_data):
     root = ET.Element("products")
 
     for prod_id, p in products_data.items():
-        main_images = p.get("images", {})
-        main_image_urls = list(main_images.values()) if isinstance(main_images, dict) else []
-
         text_fields = p.get("text_fields", {})
         title_pl = text_fields.get("name", "")
         desc_pl = clean_html_text(text_fields.get("description", ""))
@@ -92,26 +89,7 @@ def build_pigu_xml(products_data):
         desc_elem = ET.SubElement(product_elem, "long-description")
         desc_elem.text = desc_pl
 
-        # --- GŁÓWNE ZDJĘCIA PRODUKTU (W TAGU MEDIA) ---
-        media_elem = ET.SubElement(product_elem, "media")
-        
-        prod_images = list(main_image_urls)
-        if not prod_images and variants:
-            for v in variants_list:
-                v_imgs = v.get("images", {})
-                if isinstance(v_imgs, dict):
-                    prod_images.extend(list(v_imgs.values()))
-                elif isinstance(v_imgs, list):
-                    prod_images.extend(v_imgs)
-
-        for img_url in prod_images[:10]:
-            if img_url:
-                if not img_url.startswith("http"):
-                    img_url = "https://" + img_url
-                img_tag = ET.SubElement(media_elem, "image")
-                img_tag.text = img_url
-
-        # --- ZAGNIEŻDŻONE WARIANTY ---
+        # --- ZAGNIEŻDŻONE WARIANTY (Tylko dane katalogowe i kody EAN, BEZ ZDJĘĆ) ---
         colours_elem = ET.SubElement(product_elem, "colours")
         colour_elem = ET.SubElement(colours_elem, "colour")
         modifications_elem = ET.SubElement(colour_elem, "modifications")
@@ -119,10 +97,12 @@ def build_pigu_xml(products_data):
         for v in variants_list:
             modification_elem = ET.SubElement(modifications_elem, "modification")
 
+            # 1. Tytuł wariantu
             mod_title = ET.SubElement(modification_elem, "modification-title")
             v_name = v.get("name") or title_pl
             mod_title.text = v_name
 
+            # 2. Wymiary i Waga
             weight_val = v.get("weight") or p.get("weight") or 0.1
             length_val = v.get("length") or p.get("length") or 10
             height_val = v.get("height") or p.get("height") or 10
@@ -133,27 +113,11 @@ def build_pigu_xml(products_data):
             ET.SubElement(modification_elem, "height").text = str(height_val)
             ET.SubElement(modification_elem, "width").text = str(width_val)
 
+            # 3. Kody kreskowe paczki (EAN)
             v_ean = v.get("ean") or main_ean
             if v_ean:
                 pkg_barcode = ET.SubElement(modification_elem, "package-barcode")
                 pkg_barcode.text = str(v_ean)
-
-            # --- ZDJĘCIA WARIANTU (W TAGU MEDIA) ---
-            mod_media_elem = ET.SubElement(modification_elem, "media")
-            v_images = list(main_image_urls)
-            if v.get("images"):
-                v_imgs = v.get("images")
-                if isinstance(v_imgs, dict):
-                    v_images = list(v_imgs.values())
-                elif isinstance(v_imgs, list):
-                    v_images = v_imgs
-
-            for img_url in v_images[:10]:
-                if img_url:
-                    if not img_url.startswith("http"):
-                        img_url = "https://" + img_url
-                    img_tag = ET.SubElement(mod_media_elem, "image")
-                    img_tag.text = img_url
 
     xml_str = ET.tostring(root, encoding="utf-8").decode("utf-8")
     soup = BeautifulSoup(xml_str, "xml")
